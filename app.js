c2690d6b4aab7f00b64e46951a8005f672fbb07d
Anumjawaid <anumjawaid99@gmail.com>
2020-06-27 22:22:28 +0500

    first iteration

---
+++ app.js
@@ -0,0 +1,11 @@
+function getnum(num){
+    var a=(num)
+    a.value+=num
+    show(a.value)
+}
+function add(){
+
+}
+function show(value){
+    document.getElementById('te').placeholder =value;
+}
